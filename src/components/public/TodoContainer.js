606e3dd03038c60d7da496ca0cf7e1ad15740339
amankashyap004 <amankashyap004@gmail.com>
2023-01-26 21:42:59 +0530

    Todo Container icons

--- src/components/public/TodoContainer.js
+++ src/components/public/TodoContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
 export default function TodoContainer(props) {
    return (
-      <section className="bg-indigo-800 w-full rounded-xl p-4 flex items-center justify-between m-4">
-         <h2 className="text-white font-bold" type="text">
+      <section className="bg-indigo-800 text-white w-1/2 rounded-xl p-4 flex items-center justify-between m-4">
+         <span className=" font-bold" type="text">
             {props.todo}
-         </h2>
+         </span>
+         <div className="flex justify-center items-center">
+            <span className="cursor-pointer text-xl px-1">
+               <AiFillEdit />
+            </span>
+            <span className="cursor-pointer text-xl px-1">
+               <AiFillDelete />
+            </span>
+            <span className="cursor-pointer text-xl px-1">
+               <MdDone />
+            </span>
+         </div>
       </section>
    );
 }
--- src/components/public/TodoList.js
+++ src/components/public/TodoList.js
@@ -3,9 +3,14 @@ import TodoContainer from "./TodoContainer";
 
 export default function TodoList(props) {
    return (
-      <div>
+      <div className="w-full flex items-center justify-center flex-wrap">
          {props.allTodo.map((item) => (
-            <TodoContainer todo={item.todo} />
+            <TodoContainer
+               todo={item.todo}
+               key={item.id}
+               allTodo={props.allTodo}
+               setAllTodo={props.setAllTodo}
+            />
          ))}
       </div>
    );
