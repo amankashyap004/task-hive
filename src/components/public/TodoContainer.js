a4387d01758cfb158aab1b793b0238ddacad3d97
amankashyap004 <amankashyap004@gmail.com>
2023-01-27 18:49:10 +0530

    handle edit function

--- src/components/public/TodoContainer.js
+++ src/components/public/TodoContainer.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import { useState } from "react";
 
 export default function TodoContainer(props) {
+   const [edit, setEdit] = useState(false);
+   const [editTodo, setEditTodo] = useState(props.todo);
+
+   const handleEdit = (e, id) => {
+      e.preventDefault();
+      props.setAllTodo(
+         props.allTodo.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+      );
+      setEdit(false);
+   };
+
    const handleDelete = (id) => {
       props.setAllTodo(props.allTodo.filter((todo) => todo.id !== id));
    };
@@ -14,8 +26,17 @@ export default function TodoContainer(props) {
    };
 
    return (
-      <section className="bg-indigo-800 text-white w-1/2 rounded-xl p-4 flex items-center justify-between m-4">
-         {props.isDone ? (
+      <form
+         onSubmit={(e) => handleEdit(e, props.id)}
+         className="bg-indigo-800 text-white w-1/2 rounded-xl p-4 flex items-center justify-between m-4"
+      >
+         {edit ? (
+            <input
+               value={editTodo}
+               onChange={(e) => setEditTodo(e.target.value)}
+               className="w-[85%] p-2 outline-none rounded bg-transparent border-dashed border-2 border-slate-100"
+            />
+         ) : props.isDone ? (
             <s className="font-bold" type="text">
                {props.todo}
             </s>
@@ -25,7 +46,14 @@ export default function TodoContainer(props) {
             </span>
          )}
          <div className="flex justify-center items-center">
-            <span className="cursor-pointer text-xl px-1 active:text-green-200 hover:text-red-400">
+            <span
+               onClick={() => {
+                  if (!edit && !props.isDone) {
+                     setEdit(!edit);
+                  }
+               }}
+               className="cursor-pointer text-xl px-1 active:text-green-200 hover:text-red-400"
+            >
                <AiFillEdit />
             </span>
             <span
@@ -41,6 +69,6 @@ export default function TodoContainer(props) {
                <MdDone />
             </span>
          </div>
-      </section>
+      </form>
    );
 }
