5cd66a28848afa19fd261143ecfb5cd1f651b5e9
amankashyap004 <amankashyap004@gmail.com>
2023-01-29 20:25:57 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import "./App.css";
 import Header from "./components/public/Header";
 import HomePage from "./pages/public/HomePage";
 import LoginAndSignupPage from "./pages/public/LoginAndSignupPage";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
    return (
-      <div>
-         <Header />
-         {/* <HomePage /> */}
-         <LoginAndSignupPage />
-      </div>
+      <BrowserRouter>
+         <Routes>
+            <Route
+               path={`/`}
+               element={
+                  <div>
+                     <Header />
+                     <LoginAndSignupPage />
+                  </div>
+               }
+            />
+            <Route
+               path={`/home`}
+               element={
+                  <div>
+                     <Header />
+                     <HomePage />
+                  </div>
+               }
+            />
+         </Routes>
+      </BrowserRouter>
    );
 }
 
