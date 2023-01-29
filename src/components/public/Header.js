917e7d9fa3d28955c6051d96f65dbce44fabf2f3
amankashyap004 <amankashyap004@gmail.com>
2023-01-29 21:20:01 +0530

    change name TODO LIST to TASK HIVE

--- src/components/public/Header.js
+++ src/components/public/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
       <header className="w-full h-15 p-4 bg-slate-900 flex justify-between items-center fixed z-10 md:h-14 md:p-2">
          <div className="ml-4 md:ml-2 sm:ml-2">
             <Link to="/">
-               <h1 className="font-bold text-white cursor-pointer">TODO LIST</h1>
+               <h1 className="font-bold text-white cursor-pointer">Task Hive</h1>
             </Link>
          </div>
          <section className="mr-4 flex justify-between items-center md:mr-2 sm:mr-1">
