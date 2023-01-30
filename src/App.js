6d45826db4be8b5bb6ff0220a9ffdd748b5e9b18
amankashyap004 <amankashyap004@gmail.com>
2023-01-30 12:52:01 +0530

    BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SignupPage from "./pages/public/SignupPage";
 import InfoPage from "./pages/public/InfoPage";
 function App() {
    return (
-      <BrowserRouter>
+      <BrowserRouter basename="todo-list-in-react">
          <Routes>
             <Route
                path={`/`}
