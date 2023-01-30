fa7cbb829ad3ae2dc4addd3a735a23788ad3b7e8
amankashyap004 <amankashyap004@gmail.com>
2023-01-30 19:20:09 +0530

    create Long btn utils

---
+++ src/utils/LongBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function LongBtn(props) {
+   return (
+      <section>
+         <button
+            className="bg-blue-500 text-white py-3 font-bold w-full text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            type="submit"
+         >
+            {props.text}
+         </button>
+      </section>
+   );
+}
