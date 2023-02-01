d6986d985420f9e842a0ec24da36e956fc7bc45a
amankashyap004 <amankashyap004@gmail.com>
2023-02-01 18:44:51 +0530

    few more updates in info page

--- src/components/public/Info.js
+++ src/components/public/Info.js
@@ -15,18 +15,20 @@ export default function Info() {
    const listItems = todoInfos.map((todoInfo, i) => <li key={i}>{todoInfo}</li>);
    return (
       <section className="bg-blue-50 shadow-md rounded-r-lg px-8 pt-6 pb-8 md:pt-2 md:pb-6 md:px-4 md:rounded-t-none md:rounded-b-lg">
-         <samp>
-            "Get ready to take control of your tasks with Task Hive, the ultimate task management
-            solution. With its intuitive to-do list, drag and drop functionality, and secure login
-            process, you'll never lose track of your progress again. Whether you're a busy
-            professional or just someone looking for a better way to stay organized, Task Hive is
-            the answer. So why wait? Say goodbye to manual rearranging, Join the hive and watch your
-            productivity soar!"
-         </samp>
+         <div className="text-justify text-base text-violet-900 leading-6 font-semibold">
+            <samp className="font-itim">
+               "Streamline your tasks with Task Hive, the ultimate task management solution. Say
+               goodbye to manual rearranging and hello to increased productivity. Whether you're a
+               busy professional or just seeking a better organization method, Task Hive is the
+               solution you need. Don't wait, join the hive and never lose track of your progress
+               again."
+            </samp>
+         </div>
          <div className="w-full h-[2px] bg-blue-800 my-2"></div>
          <samp className="font-extrabold md:font-bold">The features of Task Hive:</samp>
          <ul className="text-sm leading-6">{listItems}</ul>
-         <div className="flex justify-center items-center mt-6 md:mt-3">
+         <div className="w-full h-[2px] bg-blue-800 my-2"></div>
+         <div className="flex justify-center items-center mt-6 md:mt-4">
             <Link to="/login">
                <SmallBtn text="Login" />
             </Link>
