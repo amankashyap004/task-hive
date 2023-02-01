4d720e3ba5f40eaf277260677c376f91edadb91c
amankashyap004 <amankashyap004@gmail.com>
2023-02-01 14:04:40 +0530

    update info contents

--- src/components/public/Info.js
+++ src/components/public/Info.js
@@ -4,26 +4,29 @@ import { Link } from "react-router-dom";
 
 export default function Info() {
    const todoInfos = [
-      "1. Increases productivity by providing a clear and organized view of tasks.",
-      "2. Enhances time management by helping prioritize and schedule tasks.",
-      "3. Reduces stress by allowing for easier tracking of progress and deadlines.",
-      "4. Improves accountability by allowing for easy monitoring of task completion.",
-      "5. Enhances goal-setting by providing a visual representation of progress towards objectives.",
+      "1. To-do list.",
+      "2. Drag and drop functionality.",
+      "3. Active/completed task tracking.",
+      "4. Login/Signup process.",
+      "5. Access to all previous tasks after logging in.",
+      "7. User-friendly interface.",
+      "8. Modify task feature.",
    ];
    const listItems = todoInfos.map((todoInfo, i) => <li key={i}>{todoInfo}</li>);
    return (
       <section className="bg-blue-50 shadow-md rounded-r-lg px-8 pt-6 pb-8 md:pt-2 md:pb-6 md:px-4 md:rounded-t-none md:rounded-b-lg">
          <samp>
-            "Experience the power of organization with our updated To-Do List project, now featuring
-            a convenient drag and drop feature. Reorder your tasks with ease and simplify your
-            workload. Say goodbye to manual rearranging and hello to a more streamlined workflow."
+            "Get ready to take control of your tasks with Task Hive, the ultimate task management
+            solution. With its intuitive to-do list, drag and drop functionality, and secure login
+            process, you'll never lose track of your progress again. Whether you're a busy
+            professional or just someone looking for a better way to stay organized, Task Hive is
+            the answer. So why wait? Say goodbye to manual rearranging, Join the hive and watch your
+            productivity soar!"
          </samp>
          <div className="w-full h-[2px] bg-blue-800 my-2"></div>
-         <samp className="font-extrabold md:font-bold">
-            A To-Do List website is important because it:
-         </samp>
+         <samp className="font-extrabold md:font-bold">The features of Task Hive:</samp>
          <ul className="text-sm leading-6">{listItems}</ul>
-         <div className="flex justify-center items-center mt-12 md:mt-6">
+         <div className="flex justify-center items-center mt-6 md:mt-3">
             <Link to="/login">
                <SmallBtn text="Login" />
             </Link>
