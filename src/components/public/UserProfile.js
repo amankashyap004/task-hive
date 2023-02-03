4518871d3db6ce2a624429633b4a2a96ddc40dd5
amankashyap004 <amankashyap004@gmail.com>
2023-02-03 16:54:34 +0530

    add some new feature in user profile

--- src/components/public/UserProfile.js
+++ src/components/public/UserProfile.js
@@ -6,7 +6,7 @@ export default function UserProfile(props) {
       <main className="absolute top-20 right-4 bg-slate-700 px-6 py-6 rounded-md z-10">
          <section className="flex justify-center items-center flex-col">
             <div className="bg-blue-100 rounded-full flex justify-center items-center h-[6rem] w-[6rem]">
-               <h1 className="text-[5rem]">A</h1>
+               <h1 className="text-[4rem] uppercase font-medium">{props.userName[0]}</h1>
             </div>
             <div className="py-4 px-8 my-4 flex justify-start items-center bg-sky-200 rounded">
                <div className="pr-2 text-blue-900">
@@ -16,7 +16,7 @@ export default function UserProfile(props) {
                   <h2 className="font-bold">Email:</h2>
                   <p></p>
                </div>
-               <div className="text-blue-900 ">
+               <div className="text-blue-900">
                   <p className="font-medium text-xs">{props.userId}</p>
                   <p className="font-medium">{props.userName}</p>
                   <p className="font-medium">-</p>
