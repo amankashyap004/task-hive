9430b8fab1b8724e497a4add9676abdc70690166
amankashyap004 <amankashyap004@gmail.com>
2023-02-04 12:51:15 +0530

    fixed user profile

--- src/components/public/UserProfile.js
+++ src/components/public/UserProfile.js
@@ -32,7 +32,7 @@ export default function UserProfile(props) {
    };
 
    return (
-      <main className="absolute top-20 right-4 bg-slate-700 px-6 py-6 rounded-md z-10 md:top-16 md:right-2 md:p-3">
+      <main className="fixed top-20 right-4 bg-slate-700 px-6 py-6 rounded-md z-10 md:top-16 md:right-2 md:p-3">
          <section className="flex justify-center items-center flex-col">
             <div className="bg-blue-100 rounded-full flex justify-center items-center h-[6rem] w-[6rem]">
                <h1 className="text-[4rem] uppercase font-medium">A</h1>
